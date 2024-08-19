df867854f7ca9af77bf1b11b2c4b5012e7b8ebab
sofiia-r <sofiia.rostkovych@tech-stack.io>
2024-08-19 11:21:12 +0300

    test: added new tests

--- tests/add-user-ui.spec.ts
+++ tests/add-user-ui.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Locator } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("https://traineeautomation.azurewebsites.net/Forms/AddUser");
@@ -10,10 +10,10 @@ test("has title", async ({ page }) => {
 test("Create button UI", async ({ page }) => {
   await page.goto("https://traineeautomation.azurewebsites.net/Forms/AddUser");
   const el = page.getByRole("button", { name: "Create" });
- 
   await expect(el).toBeVisible;
   await expect(el).toHaveClass("btn btn-primary");
   await expect(el).toHaveCSS("background-color", "rgb(13, 110, 253)");
+  // checking btn reaction to hover
   await el.hover();
   await expect(el).toHaveCSS("background-color", "rgb(11, 94, 215)");
 });
@@ -21,10 +21,10 @@ test("Create button UI", async ({ page }) => {
 test("Cancel button UI", async ({ page }) => {
   await page.goto("https://traineeautomation.azurewebsites.net/Forms/AddUser");
   const el = page.getByRole("link", { name: "Cancel" });
-  
 
   await expect(el).toHaveClass("btn btn-secondary");
   await expect(el).toHaveCSS("background-color", "rgb(108, 117, 125)");
+  // checking btn reaction to hover
   await el.hover();
   await expect(el).toHaveCSS("background-color", "rgb(92, 99, 106)");
 });
@@ -36,16 +36,39 @@ test("User name UI", async ({ page }) => {
 
   await expect(el).toBeVisible;
   await expect(placeholder).toEqual("User Name");
-  await expect(el).toHaveText("");
+  await expect(el).toHaveValue("");
 
+  //checking error msg for too short value
+  await el.fill("ab");
+  await expect(el).toHaveValue("ab");
+  const err = page.getByText("Name is too short");
+  await expect(err).toBeVisible;
+
+  //checking maximum symbols limit - 14 characters
+  await el.fill("12345678901234567890");
+  await expect(el).toHaveValue("12345678901234");
 });
+
 test("Year of Birth", async ({ page }) => {
   await page.goto("https://traineeautomation.azurewebsites.net/Forms/AddUser");
-  const el = page.getByLabel("Year of Birth");
-  const placeholder = await el.getAttribute("placeholder");
-
+  const el = page.getByPlaceholder("Year of Birth");
   await expect(el).toBeVisible;
-  await expect(placeholder).toEqual("Year of Birth");
-  await expect(el).toHaveText("");
+  await expect(el).toHaveValue("");
+});
 
+test("Gender UI", async ({ page }) => {
+  await page.goto("https://traineeautomation.azurewebsites.net/Forms/AddUser");
+  const el = page.getByLabel("Gender");
+  await expect(el).toBeVisible;
+
+  //checking default value
+  await expect(el).toHaveValue("0");
+
+  //checking option 1 - Male
+  await el.selectOption({ label: "Male" });
+  await expect(el).toHaveValue("1");
+
+  //checking option 2 - Female
+  await el.selectOption({ label: "Female" });
+  await expect(el).toHaveValue("2");
 });
