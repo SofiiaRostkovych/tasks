e12363ca4f07873bfba1911bcbbf78b64e4af222
sofiia-r <sofiia.rostkovych@tech-stack.io>
2024-08-21 16:22:02 +0300

    test: add test data

--- tests/add-user-functionality.spec.ts
+++ tests/add-user-functionality.spec.ts
@@ -2,18 +2,29 @@ import { test, expect } from "@playwright/test";
 import { generateRandomUserName } from "../helpers/generateRandomUserName";
 const validUserData = [
   {
-    userNameValue: "nб3-",
+    userNameValue: "nб3-w",
     yearOfBirthValue: "1900",
     genderValue: "Undefined",
   },
+  {
+    userNameValue: "йцу",
+    yearOfBirthValue: "2005",
+    genderValue: "Male",
+  },
+  {
+    userNameValue: "new user",
+    yearOfBirthValue: "2004",
+    genderValue: "Female",
+  },
 ];
-let url;
+
+let userDeletionUrl;
 
 test.beforeEach(async ({ page }) => {
   await page.goto("https://traineeautomation.azurewebsites.net/Forms/AddUser");
 });
 
-validUserData.forEach(({ userNameValue, yearOfBirthValue, genderValue}) => {
+validUserData.forEach(({ userNameValue, yearOfBirthValue, genderValue }) => {
   test.describe(`Verify user "${userNameValue}" creation with valid data`, () => {
     test(`Check successful creation of new user`, async ({ page }) => {
       const genderField = page.locator('xpath=//*[@id="selectGender"]');
@@ -23,22 +34,24 @@ validUserData.forEach(({ userNameValue, yearOfBirthValue, genderValue}) => {
       );
       const createBtn = page.locator("xpath=//div[4]/button");
 
-      //TODO: update locators which use Label
+      // TODO: update locators which use Label
       await genderField.selectOption({ label: "Male" });
       await userNameField.fill(userNameValue);
       await yearOfBirthField.fill(yearOfBirthValue);
       createBtn.click();
 
-      const user = await page.locator("xpath=//table[1]/tbody/tr[last()]");
-      url = await user.getByTestId("button-Delete").getAttribute("href");
-      await expect(user.getByTestId("td-YearOfBirth")).toHaveText(yearOfBirthValue);
+      const user = await page.locator('tr:has-text("' + userNameValue + '")');
+      userDeletionUrl = await user.getByTestId("button-Delete").getAttribute("href");
+      await expect(user.getByTestId("td-YearOfBirth")).toHaveText(
+        yearOfBirthValue,
+      );
       await expect(user.getByTestId("td-UserName")).toHaveText(userNameValue);
     });
 
-    test.afterEach("Delete created user", async ({ page }) => {
-      await page.goto("https://traineeautomation.azurewebsites.net" + url);
+    test.afterEach(async ({ page }) => {
+      await page.goto("https://traineeautomation.azurewebsites.net" + userDeletionUrl);
 
-      //TODO: update locators which use Name
+      // TODO: update locators which use Name
       await page.getByRole("button", { name: "Yes" }).click();
     });
   });
--- tests/add-user-ui.spec.ts
+++ tests/add-user-ui.spec.ts
@@ -42,7 +42,7 @@ test("Verify 'User Name' field design and maximum symbols limit on the 'Add User
   await expect(placeholder).toEqual("User Name");
   await expect(userNameField).toHaveValue("");
 
-  //checking maximum symbols limit - 14 characters for User Name input
+  // checking maximum symbols limit - 14 characters for User Name input
   const testStr = generateRandomUserName(20);
   await userNameField.fill(testStr);
   await expect(userNameField).toHaveValue(testStr.substring(0, 14));
@@ -55,7 +55,7 @@ test("Verify 'Year of Birth' field design and only number input on the 'Add User
   await expect(yearOfBirthField).toBeVisible;
   await expect(yearOfBirthField).toHaveValue("");
 
-  //check that non-number input is ignored be the Year of Birth field
+  // check that non-number input is ignored be the Year of Birth field
   await yearOfBirthField.click();
   await page.keyboard.insertText("!a@");
   await expect(yearOfBirthField).toHaveValue("");
@@ -66,16 +66,16 @@ test("Check Gender field design and content on the 'Add User' page", async ({
 }) => {
   const genderField = page.locator('xpath=//*[@id="selectGender"]');
   await expect(genderField).toBeVisible;
-  //TODO: update locators which use Label
-  //checking option 1 for gender input - Male
+  // TODO: update locators which use Label
+  // checking option 1 for gender input - Male
   await genderField.selectOption({ label: "Male" });
   await expect(genderField).toHaveValue("1");
 
-  //checking option 2 for gender input - Female
+  // checking option 2 for gender input - Female
   await genderField.selectOption({ label: "Female" });
   await expect(genderField).toHaveValue("2");
 
-  //checking option 0 for gender input - Undefined
+  // checking option 0 for gender input - Undefined
   await genderField.selectOption({ label: "Undefined" });
   await expect(genderField).toHaveValue("0");
 });
@@ -83,27 +83,27 @@ test("Check Gender field design and content on the 'Add User' page", async ({
 test("Verify Header design and content on the 'Add User' page", async ({
   page,
 }) => {
-  //getting first listitem of the header
+  // getting first listitem of the header
   let listitem = page.locator("xpath=//ul/li[position()<2]");
   await expect(listitem).toBeVisible;
   await expect(listitem).toHaveRole("listitem");
-  //checking the content of the child of first listitem of the header, which is a link
+  // checking the content of the child of first listitem of the header, which is a link
   let child = listitem.locator("xpath=child::*");
   await expect(child).toHaveRole("link");
   await expect(child).toContainText("Home");
   await expect(child).toHaveAttribute("href", "/");
 
-  //getting the next listitem of the header
+  // getting the next listitem of the header
   listitem = listitem.locator("xpath=following-sibling::li[1]");
   await expect(listitem).toBeVisible();
   await expect(listitem).toHaveRole("listitem");
-  //checking the content of the child of second listitem, which is a link
+  // checking the content of the child of second listitem, which is a link
   child = listitem.locator("xpath=descendant::*");
   await expect(child).toHaveRole("link");
   await expect(child).toContainText("Add User");
   await expect(child).toHaveAttribute("href", "/Forms/AddUser");
 
-  //checking the content of last listitem of the header
+  // checking the content of last listitem of the header
   child = listitem
     .locator("xpath=ancestor::ul")
     .locator('xpath=descendant::a[contains(text(),"Add Address")]');
